dd202bdd07f52aa48280a40ffa04d108b7f79f1c
STyler4 <98915469+STyler4@users.noreply.github.com>
2023-05-03 09:46:04 +1200

    Menu_v1

--- Menu_v1.py
+++ Menu_v1.py
@@ -2,3 +2,23 @@
 through the problem using different options"""
 
 import easygui
+
+cards: {}
+
+card_list = ["Stoneling", "Vexscream", "Dawnmirage", "Blazegolem", "Websnake", "Moldvine", "Vortexwing", "Rotthing",
+             "Froststep", "Wispghoul"]
+
+while True:
+    choice = easygui.buttonbox("What would you like to do?", "Monster Cards", ["Add Monster", "Find Monster",
+                                                                               "Delete Monster", "Output all", "Exit"])
+    if choice == "Add Monster":
+        name = easygui.enterbox("Enter the monsters name: ")
+
+        cards[name] = None
+
+    elif choice == "Find Monster":
+        name = easygui.enterbox("Enter the monster name you want to search for: ")
+
+        if name in cards:
+            if cards[name] is not None:
+                easygui.msgbox(f"{name}: ")
